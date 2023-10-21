4ac741af4ef1ef0c7062eb6b5cfb1fe2d2d81fe4
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-10-21 21:51:56 +0300

    Solution: TestTask.Core - Add class "Step" and add property "List<Step> Steps".

---
+++ TestTask.Core/Components/ItemsTables/Mode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using TestTask.Core.Components.ItemsTables;
+
+namespace TestTask.Core.Components
+{
+    public class Mode : Entity
+    {
+        private Mode()
+        {
+        }
+
+        public Mode(string name, int maxBottleNumber, int maxUsedTips, List<Step> steps = null)
+        {
+            Name = name != string.Empty && name != null ? Name = name : throw new ArgumentException("The step name cannot be empty.", nameof(name));
+
+            MaxBottleNumber = maxBottleNumber >= 0 ?
+                MaxBottleNumber = maxBottleNumber :
+                throw new ArgumentException("The maximum number of bottles cannot be less than zero.", nameof(maxBottleNumber));
+
+            MaxUsedTips = maxUsedTips >= 0 ?
+                MaxBottleNumber = maxUsedTips :
+                throw new ArgumentException("The maximum used of tips cannot be less than zero.", nameof(maxUsedTips));
+
+            Steps = steps;
+        }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int MaxBottleNumber { get; set; } = 0;
+
+        public int MaxUsedTips { get; set; } = 0;
+
+        public List<Step> Steps { get; set; } = null;
+    }
+}
---
+++ TestTask.Core/Components/ItemsTables/Step.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TestTask.Core.Components.ItemsTables
+{
+    public class Step : Entity
+    {
+        private Step()
+        {
+        }
+
+        public Step(int modeId, int timer, string destination, int speed, string type, int volume)
+        {
+            ModeId = modeId > 0 ? ModeId = modeId : throw new ArgumentException("Step ID greater than zero.", nameof(modeId));
+
+            Timer = timer >= 0 ? Timer = timer : throw new ArgumentException("The timer value is greater than zero.", nameof(timer));
+
+            Destination = destination;
+
+            Speed = speed >= 0 ? Speed = speed : throw new ArgumentException("The speed value is greater than zero.", nameof(speed));
+
+            Type = type != string.Empty && type != null ?
+                Type = type :
+                throw new ArgumentException("The type name cannot be empty.", nameof(type));
+
+            Volume = volume >= 0 ? Volume = volume : throw new ArgumentException("The volume cannot be less than zero.", nameof(volume));
+        }
+
+        public int ModeId { get; set; } = 0;
+
+        public int Timer { get; set; } = 0;
+
+        public string Destination { get; set; } = null;
+
+        public int Speed { get; set; } = 0;
+
+        public string Type { get; set; } = string.Empty;
+
+        public int Volume { get; set; } = 0;
+    }
+}
