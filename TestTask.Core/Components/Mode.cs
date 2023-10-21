0fa98ecc6f9b9c2fde92382a46af4dda5662d138
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-10-21 20:55:14 +0300

    Solution: TestTask.Core - Add class "Mode".

---
+++ TestTask.Core/Components/Mode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TestTask.Core.Components
+{
+    public class Mode : Entity
+    {
+        private Mode()
+        {
+        }
+
+        public Mode(string name, int maxBottleNumber, int maxUsedTips)
+        {
+            if (name == string.Empty || name == null)
+            {
+                throw new ArgumentException("The step name cannot be empty.", nameof(name));
+            }
+
+            if (maxBottleNumber < 0)
+            {
+                throw new ArgumentException("The maximum number of bottles cannot be less than zero.", nameof(maxBottleNumber));
+            }
+
+            if (maxBottleNumber < 0)
+            {
+                throw new ArgumentException("The maximum used of tips cannot be less than zero.", nameof(maxUsedTips));
+            }
+
+            Name = name;
+            MaxBottleNumber = maxBottleNumber;
+            MaxUsedTips = maxBottleNumber;
+        }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int MaxBottleNumber { get; set; } = 0;
+
+        public int MaxUsedTips { get; set; } = 0;
+    }
+}
