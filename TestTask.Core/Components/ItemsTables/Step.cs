068c7454ec6b16eadbf65e9b547681cd9591943c
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-10-21 23:27:55 +0300

    Feature/#7 (#8)
    * Solution: TestTask.Core - Add class "Mode".
    
    * Solution: TestTask.Core - Add class "Step" and add property "List<Step> Steps".
    
    * TestTask.Core - Add override methods "Equals" and "GetHashCode" in classes "Mode" and "Step".
    
    * Solution: TestTas.Core - Update class "AppDbContext" and "DbContextFactory" with the addition of new table models.

---
+++ TestTask.Core/Components/ItemsTables/Step.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace TestTask.Core.Components.ItemsTables
+{
+    public class Step : Entity
+    {
+        private Step()
+        {
+        }
+
+        public Step(int modeId, int timer, string destination, int speed, string type, int volume)
+        {
+            ModeId = modeId > 0 ? ModeId = modeId : throw new ArgumentException("Step ID greater than zero.", nameof(modeId));
+
+            Timer = timer >= 0 ? Timer = timer : throw new ArgumentException("The timer value is greater than zero.", nameof(timer));
+
+            Destination = destination;
+
+            Speed = speed >= 0 ? Speed = speed : throw new ArgumentException("The speed value is greater than zero.", nameof(speed));
+
+            Type = type != string.Empty && type != null ?
+                Type = type :
+                throw new ArgumentException("The type name cannot be empty.", nameof(type));
+
+            Volume = volume >= 0 ? Volume = volume : throw new ArgumentException("The volume cannot be less than zero.", nameof(volume));
+        }
+
+        public int ModeId { get; set; } = 0;
+
+        public int Timer { get; set; } = 0;
+
+        public string Destination { get; set; } = null;
+
+        public int Speed { get; set; } = 0;
+
+        public string Type { get; set; } = string.Empty;
+
+        public int Volume { get; set; } = 0;
+
+        public override bool Equals(object obj) => Equals(obj as Step);
+
+        public bool Equals(Step other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return other.Id == Id
+                   && other.ModeId == ModeId
+                   && other.Timer == Timer
+                   && other.Destination == Destination
+                   && other.Speed == Speed
+                   && other.Type == Type
+                   && other.Volume == Volume;
+        }
+
+        public override int GetHashCode() => Id.GetHashCode() * ModeId.GetHashCode() + Type.GetHashCode();
+    }
+}
--- TestTask.Core/Db/DbContextFactory.cs
+++ TestTask.Core/Db/DbContextFactory.cs
@@ -18,6 +18,12 @@ namespace TestTask.Core.Db
                     db.Database.ExecuteSqlCommand(
                         "CREATE TABLE IF NOT EXISTS User" +
                         "(Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,Username TEXT NOT NULL UNIQUE, PasswordHash TEXT NOT NULL)");
+                    db.Database.ExecuteSqlCommand(
+                        "CREATE TABLE IF NOT EXISTS Modes" +
+                        "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,Name TEXT NOT NULL, MaxBottleNumber INTEGER NOT NULL, MaxUsedTips INTEGER NOT NULL)");
+                    db.Database.ExecuteSqlCommand(
+                        "CREATE TABLE IF NOT EXISTS Steps" +
+                        "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,ModeId INTEGER NOT NULL, Timer INTEGER NOT NULL,Destination TEXT, Speed INTEGER NOT NULL, Type TEXT NOT NULL, Volume INTEGER NOT NULL)");
                 }
             }
         }
