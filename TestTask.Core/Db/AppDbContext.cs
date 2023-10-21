eb6c5c92377fc6145fe33c50e899fabe1130f2cd
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-10-21 23:26:18 +0300

    Solution: TestTas.Core - Update class "AppDbContext" and "DbContextFactory" with the addition of new table models.

--- TestTask.Core/Db/AppDbContext.cs
+++ TestTask.Core/Db/AppDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using TestTask.Core.Components;
+using TestTask.Core.Components.ItemsTables;
 
 namespace TestTask.Core.Db
 {
@@ -14,14 +15,37 @@ namespace TestTask.Core.Db
 
         public DbSet<User> Users { get; set; }
 
+        public DbSet<Mode> Modes { get; set; }
+
+        public DbSet<Step> Steps { get; set; }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            var configuration = modelBuilder.Entity<User>();
-            configuration.ToTable("User");
-            configuration.HasKey(e => e.Id);
-            configuration.Property(e => e.Id).HasColumnName("Id").HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
-            configuration.Property(e => e.Username).IsRequired().HasColumnName("Username").HasMaxLength(128);
-            configuration.Property(e => e.PasswordHash).IsRequired().HasColumnName("PasswordHash").HasMaxLength(128);
+            var configurationUser = modelBuilder.Entity<User>();
+            configurationUser.ToTable("User");
+            configurationUser.HasKey(e => e.Id);
+            configurationUser.Property(e => e.Id).HasColumnName("Id").HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            configurationUser.Property(e => e.Username).IsRequired().HasColumnName("Username").HasMaxLength(128);
+            configurationUser.Property(e => e.PasswordHash).IsRequired().HasColumnName("PasswordHash").HasMaxLength(128);
+
+            var configurationMode = modelBuilder.Entity<Mode>();
+            configurationMode.ToTable("Modes");
+            configurationMode.HasKey(e => e.Id);
+            configurationMode.Property(e => e.Id).HasColumnName("ID").HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            configurationMode.Property(e => e.Name).IsRequired().HasColumnName("Name").HasMaxLength(128);
+            configurationMode.Property(e => e.MaxBottleNumber).IsRequired().HasColumnName("MaxBottleNumber");
+            configurationMode.Property(e => e.MaxUsedTips).IsRequired().HasColumnName("MaxUsedTips");
+            configurationMode.HasMany(e => e.Steps).WithRequired().HasForeignKey(e => e.ModeId);
+
+            var configurationStep = modelBuilder.Entity<Step>();
+            configurationStep.ToTable("Steps");
+            configurationStep.HasKey(e => e.Id);
+            configurationStep.Property(e => e.Id).HasColumnName("ID").HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            configurationStep.Property(e => e.Timer).IsRequired().HasColumnName("Timer");
+            configurationStep.Property(e => e.Destination).HasColumnName("Destination");
+            configurationStep.Property(e => e.Speed).IsRequired().HasColumnName("Speed");
+            configurationStep.Property(e => e.Type).IsRequired().HasColumnName("Type").HasMaxLength(128);
+            configurationStep.Property(e => e.Volume).IsRequired().HasColumnName("Volume");
         }
     }
 }
