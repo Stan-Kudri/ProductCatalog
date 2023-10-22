b8d7805ca895d66941807d6dd73410d9bbc5df98
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-10-23 02:59:21 +0300

    TestTask.Core - Add reader item mode and recording the result of reading the data.

---
+++ TestTask.Core/Extension/ReadExtension/ReadItemExtension.cs
@@ -0,0 +1,73 @@
+﻿using NPOI.SS.UserModel;
+using System.Collections.Generic;
+using TestTask.Core.Components;
+using TestTask.Core.Enum;
+using TestTask.Core.ImportDB.Component;
+
+namespace TestTask.Core.Extension.ReadExtension
+{
+    public static class ReadItemExtension
+    {
+        public static Result<Mode> ReadMode(this IRow row, Dictionary<ModeField, int> columns)
+        {
+            if (row.Cells.Count < columns.Count)
+            {
+                return Result<Mode>.CreateFail("Fewer cells than needed", row.RowNum);
+            }
+
+            var res = new Mode();
+            foreach (var pair in columns)
+            {
+                switch (pair.Key)
+                {
+                    case ModeField.Id:
+
+                        var id = row.GetInt(pair.Value, "Id");
+                        if (!id.Success)
+                        {
+                            return id.ToError<Mode>();
+                        }
+                        res.Id = id.Value;
+                        break;
+
+                    case ModeField.Name:
+
+                        var name = row.GetString(pair.Value, "Name");
+                        if (!name.Success)
+                        {
+                            return name.ToError<Mode>();
+                        }
+                        if (string.IsNullOrEmpty(name.Value))
+                        {
+                            return Result<Mode>.CreateFail("Name should not be empty", row.RowNum);
+                        }
+                        res.Name = name.Value;
+                        break;
+
+                    case ModeField.MaxBottleNumber:
+
+                        var bottleNumber = row.GetInt(pair.Value, "MaxBottle");
+                        if (!bottleNumber.Success)
+                        {
+                            return bottleNumber.ToError<Mode>();
+                        }
+                        res.MaxBottleNumber = bottleNumber.Value;
+                        break;
+
+                    case ModeField.MaxUsedTips:
+
+                        var maxUsedTips = row.GetInt(pair.Value, "MaxUsedTips");
+                        if (!maxUsedTips.Success)
+                        {
+                            return maxUsedTips.ToError<Mode>();
+                        }
+                        res.MaxUsedTips = maxUsedTips.Value;
+                        break;
+                }
+
+            }
+
+            return Result<Mode>.CreateSuccess(res, row.RowNum);
+        }
+    }
+}
--- TestTask.Core/ImportDB/Read/ReadMode.cs
+++ TestTask.Core/ImportDB/Read/ReadMode.cs
@@ -3,8 +3,10 @@ using NPOI.XSSF.UserModel;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using TestTask.Core.Components;
 using TestTask.Core.Enum;
 using TestTask.Core.Extension.ReadExtension;
+using TestTask.Core.ImportDB.Component;
 
 namespace TestTask.Core.ImportDB.Read.Header
 {
@@ -12,7 +14,7 @@ namespace TestTask.Core.ImportDB.Read.Header
     {
         private const string ModeSheetName = "Modes";
 
-        public void Reader()
+        public List<Result<Mode>> Reader()
         {
             using (FileStream fileStream = new FileStream("DataExample.xlsx", FileMode.Open, FileAccess.Read))
             {
@@ -48,8 +50,24 @@ namespace TestTask.Core.ImportDB.Read.Header
                 {
                     Console.WriteLine("Failed to load title.");
                     Console.ReadLine();
-                    return;
+                    return new List<Result<Mode>>();
                 }
+
+                var addMode = new List<Result<Mode>>();
+                for (var i = 1; i <= sheet.LastRowNum; i++)
+                {
+                    IRow row = sheet.GetRow(i);
+                    if (row == null)
+                    {
+                        addMode.Add(Result<Mode>.CreateFail("Row should not be empty", row.RowNum));
+                        continue;
+                    }
+
+                    var modeResult = row.ReadMode(header);
+                    addMode.Add(modeResult);
+                }
+
+                return addMode;
             }
         }
     }
