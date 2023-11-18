ffe9caa4a60a89eb9f99edb71c6a0729e1d40c4d
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-11-18 20:43:52 +0300

    TestTask.Test: Add new project and add class creat ApDbContext in memory.

---
+++ TestTask.Test/TestDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using TestTask.Core;
+
+namespace TestTask.Test
+{
+    public class TestDbContextFactory
+    {
+        public AppDbContext Create()
+        {
+            var sqlLiteConnection = new SqliteConnection("Data Source=:memory:");
+            sqlLiteConnection.Open();
+
+            var builder = new DbContextOptionsBuilder().UseSqlite(sqlLiteConnection);
+            var dbContext = new AppDbContext(builder.Options);
+            dbContext.Database.EnsureCreated();
+
+            return dbContext;
+        }
+    }
+}
