6d48ba12bfe2fa1f5f145eb0a02b77e3139feea8
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-11-29 00:11:21 +0300

    TestTask.MudBlazors: Changed pages Registration/SignIn and add class UserModel.

---
+++ TestTask.MudBlazors/Model/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using TestTask.Core.Models.Users;
+
+namespace TestTask.MudBlazors.Model
+{
+    public class UserModel
+    {
+        private string _username = string.Empty;
+        private string _password = string.Empty;
+
+        public UserModel()
+        {
+        }
+
+        public UserModel(string username, string password)
+        {
+            _username = username;
+            _password = password;
+        }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public User ToUser() => new User(_username, _password);
+    }
+}
