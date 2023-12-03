259ec4907b2c8c657820e8b5ecf3d46b9b863d50
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-12-04 00:33:37 +0300

    Feature/#75 (#76) [Access to work in the application is available upon authorization - Not done]
    * TestTask - Add project "TestTask.MudBlazor"
    
    * TestTask.MudBlazor: Change framework 6.0 -> 8.0; Update MudBlazor; Rename and change start page.
    
    * TestTask.MudBlazor: Rename and changed page(razor) "Counter" -> "Registration";
    
    * TestTask.MudBlazor: Add button "Registration" and "Sign In".
    
    * TestTask.MudBlazor - Change Registration page.
    
    * TestTask.MudBlazor - Add "Sign In" page.
    
    * Solutin: Change all project with "net 8" to "net 7";
    
    * TestTask.MudBlazors: Changed pages Registration/SignIn and add class UserModel.
    
    * TestTask.MudBlazors - Add implementation of account registration.
    
    * TestTask.MudBlazor - Remove unnecessary code.
    
    * TestTask.MudBlazor - Add navigate page.
    
    * TestTask.MudBlazor - Add navigation and chnge name button.
    
    * TestTask.MudBlazor - Change location and validation methods.
    
    * TestTask.MudBlazor - Changed components.
    
    * TestTask.MudBlazor - Changed size and class "UserModel".
    
    * TestTask.MudBlazor - Remove unused code.

---
+++ TestTask.MudBlazors/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using MudBlazor.Services;
+using TestTask.Core;
+using TestTask.Core.Models.Companies;
+using TestTask.Core.Models.Products;
+using TestTask.Core.Models.Types;
+using TestTask.Core.Models.Users;
+using TestTask.MudBlazors.Data;
+
+const string ConnectionName = "DbConnection";
+
+var builder = WebApplication.CreateBuilder(args);
+
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddMudServices();
+builder.Services.AddSingleton(e => new DbContextFactory(ConnectionName));
+builder.Services.AddScoped(e => e.GetRequiredService<DbContextFactory>().Create());
+//builder.Services.AddScoped<IMessageBox>(e => new MessageBoxShow());
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<CompanyService>();
+builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<CategoryService>();
+builder.Services.AddScoped<ProductTypeService>();
+builder.Services.AddScoped<UserValidator>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
