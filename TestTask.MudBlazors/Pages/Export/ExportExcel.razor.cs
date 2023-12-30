f3dce285ce50f4812e3fbf41d3c62a06ede00154
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-12-30 20:04:51 +0300

    TestTask.MudBlazor - Add ExportExcel. (#97)

---
+++ TestTask.MudBlazors/Pages/Export/ExportExcel.razor.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using NPOI.XSSF.UserModel;
+using TestTask.Core.Export.SheetFillers;
+using TestTask.Core.Import;
+using TestTask.Core.Models.Categories;
+using TestTask.Core.Models.Companies;
+using TestTask.Core.Models.Products;
+using TestTask.Core.Models.Types;
+
+namespace TestTask.MudBlazors.Pages.Export
+{
+    public partial class ExportExcel
+    {
+        [Inject] private CompanyRepository CompanyRepository { get; set; } = null!;
+        [Inject] private CategoryRepository CategoryRepository { get; set; } = null!;
+        [Inject] private ProductTypeRepository ProductTypeRepository { get; set; } = null!;
+        [Inject] private ProductRepository ProductRepository { get; set; } = null!;
+        [Inject] private IJSRuntime JS { get; set; } = null!;
+
+        private async Task DownloadFile()
+        {
+            var companySheetFiller = new CompanySheetFiller(CompanyRepository);
+            var categorySheetFiller = new CategorySheetFiller(CategoryRepository);
+            var typeSheetFiller = new TypeSheetFiller(ProductTypeRepository);
+            var productSheetFiller = new ProductSheetFiller(ProductRepository);
+
+            var fillers = new ISheetFiller[]
+            {
+                    companySheetFiller,
+                    productSheetFiller,
+                    categorySheetFiller,
+                    typeSheetFiller,
+            };
+
+            var workbook = new XSSFWorkbook();
+            foreach (var filler in fillers)
+            {
+                var sheet = workbook.CreateSheet(filler.Name);
+                filler.Fill(sheet);
+            }
+
+            using var root = new MemoryStream();
+            using var XSSFStream = new NonClosableStream(root);
+            workbook.Write(XSSFStream);
+            XSSFStream.Position = 0;
+
+            byte[] bytes = root.ToArray();
+            await JS.InvokeVoidAsync("BlazorDownloadFile", "Export.xlsx", bytes);
+        }
+    }
+}
