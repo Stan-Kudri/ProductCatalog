f0ce40bac3f4c49ff708416362e29c2ba44c5f9e
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2024-01-11 21:43:52 +0300

    TestTask.Core - Rename classes.

---
+++ TestTask.Core/DataTable/Table.cs
@@ -0,0 +1,17 @@
+﻿using Ardalis.SmartEnum;
+
+namespace TestTask.Core.DataTable
+{
+    public class Table : SmartEnum<Table>
+    {
+        public static readonly Table Company = new Table("Company", 0);
+        public static readonly Table Category = new Table("Category", 1);
+        public static readonly Table TypeProduct = new Table("Type Product", 2);
+        public static readonly Table Product = new Table("Product", 3);
+
+        public Table(string name, int value)
+            : base(name, value)
+        {
+        }
+    }
+}
