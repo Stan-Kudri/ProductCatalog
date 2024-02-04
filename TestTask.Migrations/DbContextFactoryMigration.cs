ff644ede790ab03e9ccccac39f2a023f62716ea1
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2024-02-05 00:19:05 +0300

    TestTask.Migrations - Add first migration in project.

---
+++ TestTask.Migrations/DbContextFactoryMigration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using TestTask.Core;
+
+namespace TestTask.Migrations
+{
+    public class DbContextFactoryMigration : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        private const string ConnectionName = "DbConnection";
+
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder().UseSqlite($"Data Source={ConnectionName}.db", b => b.MigrationsAssembly(typeof(DbContextFactoryMigration).Assembly.FullName));
+            var dbContext = new AppDbContext(builder.Options);
+            return dbContext;
+        }
+    }
+}
