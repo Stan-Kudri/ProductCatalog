b36c8916c96704de43b2757adb203d7d64900ee7
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2024-02-05 23:46:19 +0300

    TestTask.MudBlazor - Add class used migration "FileDbContextFactory" in project TestTask.MudBlzor.

---
+++ TestTask.MudBlazors/DbContext/FileDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Core;
+using TestTask.Migrations;
+
+namespace TestTask.MudBlazors.DbContext
+{
+    public class FileDbContextFactory
+    {
+        public string _connectionName;
+
+        public FileDbContextFactory(string connectionName) => _connectionName = connectionName;
+
+        public AppDbContext Create()
+        {
+            var builder = new DbContextOptionsBuilder<AppDbContext>().UseSqlite($"Data Source={_connectionName}.db", x =>
+            {
+                x.MigrationsAssembly(typeof(DbContextFactoryMigration).Assembly.FullName);
+            });
+
+            var dbContext = new AppDbContext(builder.Options);
+            dbContext.Database.Migrate();
+
+            return dbContext;
+        }
+    }
+}
