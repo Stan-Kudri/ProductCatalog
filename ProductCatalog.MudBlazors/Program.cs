2cc72d72d2bb6b0ed41fd27cd0e5812bbf719034
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2025-09-17 00:08:12 +0300

    ProductCatalog.MudBlazor - Add Electron to the window "MudBlazor" app.

--- ProductCatalog.MudBlazors/Program.cs
+++ ProductCatalog.MudBlazors/Program.cs
@@ -1,7 +1,11 @@
+using ElectronNET.API;
 using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 using ProductCatalog.MudBlazors.Extension;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Electron.NET
+builder.WebHost.UseElectron(args);
 
 StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
 
@@ -17,4 +21,12 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.UseAppPipeline();
-app.Run();
+
+await app.StartAsync();
+
+if (HybridSupport.IsElectronActive)
+{
+    await Electron.WindowManager.CreateWindowAsync();
+}
+
+await app.WaitForShutdownAsync();
